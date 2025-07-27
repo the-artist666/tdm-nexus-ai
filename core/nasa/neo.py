c84248d8dc426123e2f5160ebabf400286600205
the-artist666 <136616201+the-artist666@users.noreply.github.com>
2025-07-27 23:00:49 +0100

    Update neo.py

--- core/nasa/neo.py
+++ core/nasa/neo.py
@@ -1,1 +1,13 @@
+import requests
+from datetime import datetime
 
+def fetch_neo_count(api_key):
+    today = datetime.now().strftime("%Y-%m-%d")
+    url = "https://api.nasa.gov/neo/rest/v1/feed"
+    params = {"start_date": today, "end_date": today, "api_key": api_key}
+    try:
+        r = requests.get(url, params=params, timeout=5)
+        data = r.json()
+        return sum(len(data["near_earth_objects"][date]) for date in data["near_earth_objects"])
+    except:
+        return "N/A"
